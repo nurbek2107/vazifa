d078a8f44587dfa684d8fdb320b3c0077b8af32f
nurbek2107 <abduaxatov002@gmail.com>
2024-04-20 21:04:22 +0500

    main.js
    Signed-off-by: nurbek2107 <abduaxatov002@gmail.com>

---
+++ main.js
@@ -0,0 +1,13 @@
+//// 1-misol
+
+// function getLevel2(n) {
+//     let result = [];
+//     for (let i = 2; i <= n; i++) {
+//         result.push((2 ** (i - 1)));
+//     }
+//     return result;
+// }
+// console.log(getLevel2(5));
+
+////2-misol
+
